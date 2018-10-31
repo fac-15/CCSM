b3b321189afa7d93f82c40d8d04dc4ff19ab0506
Michele <boimik87@gmail.com>
2018-10-31 17:14:33 +0000

    Navbar - DAY 1

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.getElementById('hamburger').addEventListener('click', myHamburger, false)
 
+function myHamburger() {
+// alert("you clicked the hamburger");
+document.getElementById('navMenu').style.display = "block";
+document.getElementById('hamburger').style.display = "none";
+
+    
+}
+
+document.body.addEventListener("click", reverseFunction, false);
+
+// function reverseFunction(event){
+//     if
+// }
