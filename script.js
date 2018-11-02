058020cb90321ddcc231628a2f14b09d3eaba248
charlie la fosse <charlielafosse@outlook.com>
2018-11-02 12:32:54 +0000

    smooth scroll feature

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 document.body.addEventListener("click", clickFunction, false);
 
+
 function clickFunction(event){
+    const homeNav = event.target.id == "homeNav";
+    const aboutNav = event.target.id == "aboutNav";
+    const teamNav = event.target.id == "teamNav";
+    const contactNav = event.target.id == "contactNav";
+
     if(event.target.id == "hamburger"){
       document.getElementById('navMenu').style.display = "block";
       document.getElementById('hamburger').style.display = "none";
     } else if(event.target.id == "navMenu"){
+    } else if(homeNav){
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    } else if(aboutNav){
+      window.scrollTo({
+        top: 666,
+        behavior: "smooth"
+      });
+    } else if(teamNav){
+      window.scrollTo({
+        top: 1266,
+        behavior: "smooth"
+      });
+    } else if(contactNav){
+      window.scrollTo({
+        top: 2980,
+        behavior: "smooth"
+      });
     } else if (event.target.id == 'close') {
       document.getElementById('navMenu').style.display = 'none';
       document.getElementById('hamburger').style.display = 'block';
@@ -13,6 +39,10 @@ function clickFunction(event){
       document.getElementById('hamburger').style.display = 'block';
     }
 }
+
+// document.getElementById("navMenu").addEventListener("click", navFunction, false);
+//
+
 
 let emailRegex = /[A-Za-z0-9_.]+@[A-Za-z0-9_.]{3,}\.{1,1}[a-z]{2,}$/;
 let textRegex = /[\w]{1,}/;
